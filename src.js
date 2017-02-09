22d19f39ac090b2cd2d1697eba83d43a5e6c9a15
zahraayman <zahra_zb1@hotmail.com>
2017-02-09 09:50:55 +0200

    add pause function

--- src.js
+++ src.js
@@ -1,39 +1,49 @@
-
 var minutes = 0;
 var seconds = 0;
 var setTime;
-var timerStatus = 0;
+var timerStatus = 1;
 
 function program() {
     var start = Date.now(),
-      duration,
+      duration = document.getElementById('timeDuration').value * 60  ,
       diff;
 	var programInterface = {
 		startTimer : function(){
-      duration = document.getElementById('timeDuration').value * 60 ;
+
 		  if(duration < 0 ){
        alert("Enter valid number");
       }else{
-				diff =  (((Date.now() - start) / 1000) | 0);
-      //  document.getElementById("minutes").value = minutes;
-                minutes = (diff / 60) | 0;
-                seconds = (diff % 60) | 0;
-                minutes = minutes < 10 ? "0" + minutes : minutes;
-                seconds = seconds < 10 ? "0" + seconds : seconds;
-                document.getElementById("minutes").innerHTML = minutes;
-                document.getElementById("seconds").innerHTML = seconds;
+				  diff =  duration - (((Date.now() - start) / 1000) | 0);
+          minutes = (diff / 60) | 0;
+          seconds = (diff % 60) | 0;
+          minutes = minutes < 10 ? "0" + minutes : minutes;
+          seconds = seconds < 10 ? "0" + seconds : seconds;
+          document.getElementById("minutes").innerHTML = minutes;
+          document.getElementById("seconds").innerHTML = seconds;
           }
-                if (diff <= 0) {
-                      clearTimeout(setTime);
-                }else{
-                    setTime = setTimeout(programInterface.startTimer, 1000);
-                  }},
+          if (diff <= 0) {
+                clearTimeout(setTime);
+          }else{
+              setTime = setTimeout(programInterface.startTimer, 1000);
+            }},
 		reset : function() {
 			clearTimeout(setTime);
 			minutes = "00";
 			seconds = "00";
       document.getElementById("minutes").innerHTML = minutes;
       document.getElementById("seconds").innerHTML = seconds;
+		},
+    paus : function(){
+			if(timerStatus == 1){
+				timerStatus = 0;
+		    clearTimeout(setTime);
+			}
+			else{
+				timerStatus = 1;
+				duration = (minutes * 60 ) + seconds;
+				start = Date.now();
+				setTimeout(programInterface.startTimer, 1000);
+			}
 		}
   }
   return programInterface;
