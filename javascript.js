b8c03a9a10c4e413ad3b9aa8cf66923e8101b146
Alaa-Khattab <alaakhattab92@gmail.com>
2017-02-11 22:45:24 +0200

    edit indentation

--- javascript.js
+++ javascript.js
@@ -3,50 +3,50 @@ var seconds = 0;
 var setTime;
 var timerStatus = 1;
 function program(){
-    var start = Date.now();
-    var duration = document.getElementById('timeDuration').value * 60;
-    var diff;
+	var start = Date.now();
+	var duration = document.getElementById('timeDuration').value * 60;
+	var diff;
 	var programInterface = {
-		startTimer : function(){
-		    if(duration < 0){
-                alert("Enter valid number");
-            }
-            else{
-	            diff =  duration - (((Date.now() - start) / 1000) | 0);
-				minutes = (diff / 60) | 0;
-				seconds = (diff % 60) | 0;
-				minutes = minutes < 10 ? "0" + minutes : minutes;
-				seconds = seconds < 10 ? "0" + seconds : seconds;
-				document.getElementById("minutes").innerHTML = minutes;
-				document.getElementById("seconds").innerHTML = seconds;
-	            document.getElementById("strBtn").disabled = true;
-            }
-            if(diff < 0){
-                clearTimeout(setTime);
-            }else{
-                setTime = setTimeout(programInterface.startTimer, 1000);
-            }
-        },
-		reset : function() {
+	startTimer : function(){
+		if(duration < 0){
+			alert("Enter valid number");
+		}
+		else{
+			diff =  duration - (((Date.now() - start) / 1000) | 0);
+			minutes = (diff / 60) | 0;
+			seconds = (diff % 60) | 0;
+			minutes = minutes < 10 ? "0" + minutes : minutes;
+			seconds = seconds < 10 ? "0" + seconds : seconds;
+			document.getElementById("minutes").innerHTML = minutes;
+			document.getElementById("seconds").innerHTML = seconds;
+			document.getElementById("strBtn").disabled = true;
+		}
+		if(diff < 0){
+			clearTimeout(setTime);
+		}else{
+			setTime = setTimeout(programInterface.startTimer, 1000);
+		}
+	},
+	reset : function() {
 			clearTimeout(setTime);
 			minutes = "00";
 			seconds = "00";
-    	    document.getElementById("minutes").innerHTML = minutes;
-            document.getElementById("seconds").innerHTML = seconds;
-	        document.getElementById("strBtn").disabled = false;
-		},
-        paus : function(){
-			if(timerStatus == 1){
-				timerStatus = 0;
-		    clearTimeout(setTime);
-			}
-			else{
-				timerStatus = 1;
-				duration = (minutes * 60 ) + seconds;
-				start = Date.now();
-				setTimeout(programInterface.startTimer, 1000);
-			}
+			document.getElementById("minutes").innerHTML = minutes;
+			document.getElementById("seconds").innerHTML = seconds;
+			document.getElementById("strBtn").disabled = false;
+	},
+	paus : function(){
+		if(timerStatus == 1){
+			timerStatus = 0;
+			clearTimeout(setTime);
 		}
-  }
+		else{
+			timerStatus = 1;
+			duration = (minutes * 60 ) + seconds;
+			start = Date.now();
+			setTimeout(programInterface.startTimer, 1000);
+		}
+	}
+}
 return programInterface;
 }
